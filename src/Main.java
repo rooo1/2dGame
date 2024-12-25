4ecadf083bd2a86e23e8bb5d6b5815fdd788079a
Rohan <rohanr@digitalprudentia.com>
2024-12-25 20:07:45 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+    private final int WIDTH = 1000;
+    private final int HEIGHT = 800;
+
     public static void main(String[] args) {
+        launch(args);
+    }
 
-            System.out.println("Welcome to my 2D game");
+    @Override
+    public void start(Stage primaryStage) {
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        // Start the game loop
+        GameLoop loop = new GameLoop(gc);
+        loop.start();
+
+        StackPane root = new StackPane(canvas);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        primaryStage.setTitle("JavaFX Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
